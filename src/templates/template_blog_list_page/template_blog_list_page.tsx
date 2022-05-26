e0951153f4785b7e925d12d520d3754449077cbc
tl-alex-mcgovern <alex.mcgovern@truelayer.com>
2022-05-27 00:07:37 +0100

    refactor(blog list template): migrate to TS

--- src/templates/template_blog_list_page/template_blog_list_page.tsx
+++ src/templates/template_blog_list_page/template_blog_list_page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import PropTypes from "prop-types";
-import Layout from "../../components/layout/layout/layout.tsx";
+import Layout from "../../components/layout/layout/layout";
 import LayoutMaxWidthContainer from "../../components/layout/layout_max_width_container/layout_max_width_container";
 import LayoutSectionOuter from "../../components/layout/layout_section_outer/layout_section_outer";
 import SectionBlogPostList from "../../components/molecules/blog/section_blog_articles_list/section_blog_articles_list";
@@ -9,7 +8,36 @@ import Pagination from "../../components/molecules/pokedex/pagination/pagination
 
 const PAGINATION_BASE_PATH = "blog";
 
-export default function TemplateBlogListPage({ data, pageContext }) {
+interface TemplateBlogListPageProps {
+  data: {
+    allLanguagesISO: {
+      distinct?: string[];
+    }[];
+    allMarkdownRemark: {
+      nodes?: {
+        posts?: {}[];
+      }[];
+    };
+    allPokemon: {
+      nodes?: {}[];
+    };
+    site: {
+      siteMetadata?: {
+        title?: string;
+      };
+    };
+  };
+  pageContext: {
+    currentPage?: number;
+    languageISO?: string;
+    pageCount?: number;
+  };
+}
+
+export default function TemplateBlogListPage({
+  data,
+  pageContext,
+}: TemplateBlogListPageProps) {
   const { currentPage, pageCount } = pageContext;
 
   const siteTitle = data.site.siteMetadata?.title || `Title`;
@@ -35,36 +63,6 @@ export default function TemplateBlogListPage({ data, pageContext }) {
     </Layout>
   );
 }
-
-TemplateBlogListPage.propTypes = {
-  data: PropTypes.shape({
-    allLanguagesISO: PropTypes.arrayOf(
-      PropTypes.shape({
-        distinct: PropTypes.arrayOf(PropTypes.string),
-      })
-    ),
-    allMarkdownRemark: PropTypes.shape({
-      nodes: PropTypes.arrayOf(
-        PropTypes.shape({
-          posts: PropTypes.arrayOf(PropTypes.shape({})),
-        })
-      ),
-    }),
-    allPokemon: PropTypes.shape({
-      nodes: PropTypes.arrayOf(PropTypes.shape({})),
-    }),
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string,
-      }),
-    }),
-  }).isRequired,
-  pageContext: PropTypes.shape({
-    currentPage: PropTypes.number,
-    languageISO: PropTypes.string,
-    pageCount: PropTypes.number,
-  }).isRequired,
-};
 
 export const query = graphql`
   query TemplateBlogListPageQuery($itemsPerPage: Int, $itemsToSkip: Int) {
