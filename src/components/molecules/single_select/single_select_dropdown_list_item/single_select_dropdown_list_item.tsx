826a07995689e41d87781324c8a46365203c5471
tl-alex-mcgovern <alex.mcgovern@truelayer.com>
2022-05-27 00:48:04 +0100

    refactor(single select dropdown): migrate to TS

--- src/components/molecules/single_select/single_select_dropdown_list_item/single_select_dropdown_list_item.tsx
+++ src/components/molecules/single_select/single_select_dropdown_list_item/single_select_dropdown_list_item.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import classNames from "classnames";
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import * as classes from "./single_select_dropdown_list_item.module.scss";
+
+interface SingleSelectDropdownListItemProps {
+  getItemProps(...args: unknown[]): unknown;
+  highlightedIndex?: number;
+  index: number;
+  item: {
+    link: string;
+    value: string;
+  };
+  selectedItem?: {};
+}
 
 export default function SingleSelectDropdownListItem({
   getItemProps,
@@ -10,7 +20,7 @@ export default function SingleSelectDropdownListItem({
   index,
   item,
   selectedItem,
-}) {
+}: SingleSelectDropdownListItemProps) {
   const linkClassNames = classNames(classes.result_link, {
     [classes.result_is_highlighted]: highlightedIndex === index,
     [classes.result_is_selected]: selectedItem === item,
@@ -31,20 +41,8 @@ export default function SingleSelectDropdownListItem({
   );
 }
 
-SingleSelectDropdownListItem.propTypes = {
-  getItemProps: PropTypes.func,
-  highlightedIndex: PropTypes.number,
-  index: PropTypes.number,
-  item: PropTypes.shape({
-    link: PropTypes.string,
-    value: PropTypes.string,
-  }),
-  selectedItem: PropTypes.shape({}),
-};
-
 SingleSelectDropdownListItem.defaultProps = {
   getItemProps: () => {},
-
   index: null,
   highlightedIndex: null,
   selectedItem: null,
