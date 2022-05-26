68f37d2062dc0f99c2d0c3bebab8c714dd92229e
tl-alex-mcgovern <alex.mcgovern@truelayer.com>
2022-05-27 00:31:07 +0100

    refactor(max width container): migrate to TS

--- src/components/layout/layout_max_width_container/layout_max_width_container.tsx
+++ src/components/layout/layout_max_width_container/layout_max_width_container.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 import * as classes from "./layout_max_width_container.module.scss";
 
-export default function LayoutMaxWidthContainer({ children }) {
+interface LayoutMaxWidthContainerProps {
+  children?: React.ReactNode;
+}
+
+export default function LayoutMaxWidthContainer({
+  children
+}: LayoutMaxWidthContainerProps) {
   return (
     <section className={classes.layout_max_width_container}>{children}</section>
   );
 }
-
-LayoutMaxWidthContainer.propTypes = {
-  children: PropTypes.node,
-};
 
 LayoutMaxWidthContainer.defaultProps = {
   children: null,
