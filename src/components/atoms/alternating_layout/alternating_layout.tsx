ff39cbfa4f7b6351a77a16c15e5729d4845098fd
tl-alex-mcgovern <alex.mcgovern@truelayer.com>
2022-05-28 12:51:06 +0100

    improve(alt. layout): add layout options

--- src/components/atoms/alternating_layout/alternating_layout.tsx
+++ src/components/atoms/alternating_layout/alternating_layout.tsx
@@ -4,16 +4,20 @@ import * as classes from "./alternating_layout.module.scss";
 
 interface AlternatingLayoutProps {
   children?: React.ReactNode;
-  ratio?: "2_1" | "1_2";
+  ratio?: "1_2" | "1_3" | "2_1" | "3_1" | "5_7" | "7_5";
 }
 
 export default function AlternatingLayout({
   children,
-  ratio
+  ratio,
 }: AlternatingLayoutProps) {
   const alternatingLayoutClassNames = classNames(classes.alternating_layout, {
-    [classes.ratio_2_1]: ratio === "2_1",
     [classes.ratio_1_2]: ratio === "1_2",
+    [classes.ratio_1_3]: ratio === "1_3",
+    [classes.ratio_2_1]: ratio === "2_1",
+    [classes.ratio_3_1]: ratio === "3_1",
+    [classes.ratio_5_7]: ratio === "5_7",
+    [classes.ratio_7_5]: ratio === "7_5",
   });
   return <div className={alternatingLayoutClassNames}>{children}</div>;
 }
