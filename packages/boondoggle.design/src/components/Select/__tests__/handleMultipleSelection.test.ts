37288fc791fb97ed4a08a2de80575556e6bc950b
Alex McGovern <alex_mcgovernsmith@icloud.com>
2022-11-24 19:21:53 +0000

    fix(boondoggle/select): correct formatting bug

--- packages/boondoggle.design/src/components/Select/__tests__/handleMultipleSelection.test.ts
+++ packages/boondoggle.design/src/components/Select/__tests__/handleMultipleSelection.test.ts
@@ -1,5 +1,5 @@
 import { cleanup } from "@testing-library/react";
-import { handleMultipleSelection } from "../handleMultipleSelection";
+import { handleMultipleSelection } from "../utils/handleMultipleSelection";
 
 afterEach(cleanup);
 
@@ -7,6 +7,7 @@ test("When called with an already selected item, removes it from `selectedItems`
   expect(
     handleMultipleSelection({
       newSelectedItem: { value: "foo", label: "foo" },
+      isItemSelected: true,
       selectedItems: [
         { value: "foo", label: "foo" },
         { value: "bar", label: "bar" },
@@ -18,6 +19,7 @@ test("When called with a new item, adds it to `selectedItems`", () => {
   expect(
     handleMultipleSelection({
       newSelectedItem: { value: "new", label: "new" },
+      isItemSelected: false,
       selectedItems: [
         { value: "foo", label: "foo" },
         { value: "bar", label: "bar" },
