a67ad6c7df7d1629823ad511cb7ebb2249bb4cfb
Alex McGovern <alex_mcgovernsmith@icloud.com>
2022-11-25 02:22:00 +0000

    fix(github actions): expose frontend env vars

--- packages/demo-feedback-form/src/utils/upload_mock_data.ts
+++ packages/demo-feedback-form/src/utils/upload_mock_data.ts
@@ -13,14 +13,14 @@
 // import { COMMENTS_MOCK_DATA_SET } from "../../__mocks__/COMMENTS_MOCK_DATASET";
 
 // const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_WEB_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//   apiKey: process.env.GATSBY_FIREBASE_WEB_API_KEY,
+//   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+//   projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.GATSBY_FIREBASE_APP_ID,
+//   measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
 // };
 
 // const firebaseApp = initializeApp(firebaseConfig);
--- packages/firebase/src/context/FirebaseContext.tsx
+++ packages/firebase/src/context/FirebaseContext.tsx
@@ -9,14 +9,14 @@ import { getFirestore } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_WEB_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.GATSBY_FIREBASE_WEB_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
