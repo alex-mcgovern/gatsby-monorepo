392b402d1359e81dbffb02c7b1b5d54bffa9f446
Alex McGovern <alex_mcgovernsmith@icloud.com>
2022-11-25 00:06:06 +0000

    chore(jest config): clean up

--- jest.config.ts
+++ jest.config.ts
@@ -3,25 +3,20 @@
  * ------------------------------------------------------------------------------- */
 
 const BASE_CONFIG = {
-  moduleNameMapper: {
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/test/__mocks__/file-mock.js`,
-  },
+  // moduleNameMapper: {
+  //   ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+  // },
   preset: "ts-jest",
-  reporters: ["default", "jest-junit"],
-  resetMocks: false,
-  setupFiles: [
-    // `<rootDir>/test/loadershim.js`, // "<rootDir>/test/register-context.js",
-    // "jest-localstorage-mock",
-  ],
+
   setupFilesAfterEnv: ["<rootDir>/test/setup_test_env.ts"],
   testEnvironmentOptions: {
-    url: `http://localhost`,
+    url: "http://localhost",
   },
   testPathIgnorePatterns: [
-    `node_modules`,
-    `\\.cache`,
-    `<rootDir>.*/public`,
-    `cypress`,
+    "node_modules",
+    "\\.cache",
+    "<rootDir>.*/public",
+    "cypress",
   ],
   transform: {
     "\\.css\\.ts$": "@vanilla-extract/jest-transform",
@@ -32,7 +27,6 @@ const BASE_CONFIG = {
       },
     ],
   },
-  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script)/)`],
 };
 
 /** -----------------------------------------------------------------------------
@@ -50,8 +44,6 @@ const jestConfig = {
       testMatch: [
         "<rootDir>/packages/boondoggle.design/**/?(*.)+(spec|test).[jt]s?(x)",
       ],
-
-      // resolver: "jest-node-exports-resolver",
     },
     /** ---------------------------------------------
      * 🏚 Main gatsby site
@@ -79,33 +71,6 @@ const jestConfig = {
 
     // },
   ],
-
-  // transformIgnorePatterns: ["node_modules/(?!(firebase/app|@firebase))"],
-
-  // globalSetup: "./global-setup.js",
-  // moduleNameMapper: {
-  //   ...packages.reduce((acc, name) => {
-  //     return {
-  //       ...acc,
-  //       [`@alexmcgovern/${name}(.*)$`]: `<rootDir>/packages/./${name}/$1`,
-  //     };
-  //   }, {}),
-  // },
 };
 
 export default jestConfig;
-
-// import { lstatSync, readdirSync } from "fs";
-
-// import path from "path";
-
-/** -----------------------------------------------------------------------------
- * Get listing of packages in the mono repo
- * Adapted from:
- * https://blog.ah.technology/a-guide-through-the-wild-wild-west-of-setting-up-a-mono-repo-part-2-adding-jest-with-a-breeze-16e08596f0de
- * ------------------------------------------------------------------------------- */
-
-// const basePath = path.resolve(__dirname, "packages");
-// const packages = readdirSync(basePath).filter((name) => {
-//   return lstatSync(path.join(basePath, name)).isDirectory();
-// });
