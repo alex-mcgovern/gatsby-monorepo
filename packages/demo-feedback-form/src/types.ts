c9238758f5057fc389da140ac62b1a1d38feb246
Alex McGovern <alex_mcgovernsmith@icloud.com>
2022-11-28 09:40:18 +0000

    chore(feedback form): fix types

--- packages/demo-feedback-form/src/types.ts
+++ packages/demo-feedback-form/src/types.ts
@@ -1,6 +1,6 @@
-import type { DocumentReference } from "firebase/firestore";
+import type { DocumentData, DocumentReference } from "firebase/firestore";
 
-export interface CommentShape {
+export interface CommentShape extends DocumentData {
   displayName: string | null;
   email: string;
   created: {
