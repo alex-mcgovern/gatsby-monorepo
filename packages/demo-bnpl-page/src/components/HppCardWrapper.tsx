f8be3f0100387382763800b4c5042d1711bee6f6
tl-alex-mcgovern <alex.mcgovern@truelayer.com>
2022-12-01 13:21:01 +0000

    working commit

---
+++ packages/demo-bnpl-page/src/components/HppCardWrapper.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box, { ReactNode }, Card, Icon } from "@alexmcgovern/boondoggle.design";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+interface HppCardWrapperProps {
+  title: string;
+  children: ReactNode;
+}
+
+export const HppCardWrapper = ({ title, children }: HppCardWrapperProps) => {
+  return (
+    <Card>
+      <Box>
+        <Box as="h1">{title}</Box>
+        <Icon icon={faTimes} />
+      </Box>
+      {children}
+    </Card>
+  );
+};
